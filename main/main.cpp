4534264a6ac78ccc5ecf7b1d6300b588c2f52640
Rachid Fahmi <timgrade.fahmi@gmail.com>
2026-02-20 06:42:07 -0500

    Add MissionTaskGenerator atomic

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <limits>
+#include "include/test/test_mtg.hpp"
+#include <cadmium/simulation/root_coordinator.hpp>
+#include <cadmium/simulation/logger/stdout.hpp>
+#include <cadmium/simulation/logger/csv.hpp>
+
+using namespace cadmium;
 
 int main() {
-    std::cout << "UAV_Swarm build OK\n";
+    auto model = std::make_shared<TestMTG>("test_mtg");
+    auto rootCoordinator = RootCoordinator(model);
+    rootCoordinator.setLogger<STDOUTLogger>(";");
+    rootCoordinator.start();
+    rootCoordinator.simulate(200.0);
+    rootCoordinator.stop();
     return 0;
 }
